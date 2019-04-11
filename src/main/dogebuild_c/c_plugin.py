dd52687b8ef0eddf44b181878b026601012c96c4
Kirill Sulim <kirillsulim@gmail.com>
2019-04-12 00:04:43 +0500

    Refactor

--- src/main/dogebuild_c/c_plugin.py
+++ src/main/dogebuild_c/c_plugin.py
@@ -3,6 +3,7 @@ from enum import Enum, unique, auto
 from dogebuild.plugins import DogePlugin
 import os
 import shutil
+from typing import List, Tuple
 
 
 @unique
@@ -14,6 +15,14 @@ class BinaryType(Enum):
 
 class CPlugin(DogePlugin):
     NAME = 'c-plugin'
+
+    ALLOWED_CODE_EXTENSIONS = [
+        '.c',
+    ]
+
+    ALLOWED_HEADER_EXTENSIONS = [
+        '.h',
+    ]
 
     def __init__(self, **kwargs):
         super(CPlugin, self).__init__()
@@ -32,32 +41,11 @@ class CPlugin(DogePlugin):
         self.build_dir = kwargs.get('build_dir', 'build')
 
     def compile(self) -> int:
-
         if not os.path.exists(self.build_dir):
             os.mkdir(self.build_dir)
 
         if self.type is BinaryType.STATIC_LIBRARY:
-            command = ['gcc']
-
-            o_files = []
-
-            for src in self.src:
-                command.append('-c')
-                command.append(src)
-                command.append('-o')
-
-                if src.endswith('.c'):
-                    src = src[:-2] + '.o'
-
-                file_path = os.path.join(self.build_dir, src)
-                dir_path = os.path.dirname(file_path)
-                if not os.path.exists(dir_path):
-                    os.makedirs(dir_path)
-
-                command.append(file_path)
-                o_files.append(file_path)
-
-            code = call(command)
+            code, o_files = self._compile(self.build_dir, self.src, self.type)
             if code:
                 return code
 
@@ -67,39 +55,12 @@ class CPlugin(DogePlugin):
             if code:
                 return code
 
-            for header in self.headers:
-                file_path = os.path.join(self.build_dir, 'headers', header)
-                dir_path = os.path.dirname(file_path)
-                if not os.path.exists(dir_path):
-                    os.makedirs(dir_path)
-
-                shutil.copyfile(header, file_path)
+            self._copy_header(self.build_dir, self.headers)
 
             return 0
 
         elif self.type is BinaryType.DYNAMIC_LIBRARY:
-            command = ['gcc']
-
-            o_files = []
-
-            for src in self.src:
-                command.append('-c')
-                command.append('-fPIC')  # Need to create macros in binary code. See https://renenyffenegger.ch/notes/development/languages/C-C-plus-plus/GCC/options/f/pic/index
-                command.append(src)
-                command.append('-o')
-
-                if src.endswith('.c'):
-                    src = src[:-2] + '.o'
-
-                file_path = os.path.join(self.build_dir, src)
-                dir_path = os.path.dirname(file_path)
-                if not os.path.exists(dir_path):
-                    os.makedirs(dir_path)
-
-                command.append(file_path)
-                o_files.append(file_path)
-
-            code = call(command)
+            code, o_files = self._compile(self.build_dir, self.src, self.type)
             if code:
                 return code
 
@@ -109,13 +70,7 @@ class CPlugin(DogePlugin):
             if code:
                 return code
 
-            for header in self.headers:
-                file_path = os.path.join(self.build_dir, 'headers', header)
-                dir_path = os.path.dirname(file_path)
-                if not os.path.exists(dir_path):
-                    os.makedirs(dir_path)
-
-                shutil.copyfile(header, file_path)
+            self._copy_header(self.build_dir, self.headers)
 
             return 0
 
@@ -139,6 +94,48 @@ class CPlugin(DogePlugin):
         shutil.rmtree(self.build_dir)
 
     @staticmethod
+    def _compile(build_dir: str, src_list: List[str], type: BinaryType) -> Tuple[int, List[str]]:
+        command = ['gcc']
+
+        o_files = []
+
+        for src in src_list:
+            command.append('-c')
+            if type is BinaryType.DYNAMIC_LIBRARY:
+                # Need to create macros in binary code.
+                # See https://renenyffenegger.ch/notes/development/languages/C-C-plus-plus/GCC/options/f/pic/index
+                command.append('-fPIC')
+            command.append(src)
+            command.append('-o')
+
+            base, extension = os.path.splitext(src)
+
+            if extension in CPlugin.ALLOWED_CODE_EXTENSIONS:
+                src = base + '.o'
+            else:
+                print('Warn: not allowed code file extension {} in file {}'.format(extension, src))
+
+            file_path = os.path.join(build_dir, src)
+            CPlugin._ensure_directory_exists(file_path)
+
+            command.append(file_path)
+            o_files.append(file_path)
+
+        return call(command), o_files
+
+    @staticmethod
+    def _copy_header(build_dir: str, header_list: List[str]):
+        for header in header_list:
+            base, extension = os.path.splitext(header)
+            if extension not in CPlugin.ALLOWED_HEADER_EXTENSIONS:
+                print('Warn: not allowed header file extension {} in file {}'.format(extension, header))
+
+            file_path = os.path.join(build_dir, 'headers', header)
+            CPlugin._ensure_directory_exists(file_path)
+
+            shutil.copyfile(header, file_path)
+
+    @staticmethod
     def _resolve_out_name(type: BinaryType, name: str):
         if os.name is 'posix':
             if type is BinaryType.STATIC_LIBRARY:
@@ -154,3 +151,10 @@ class CPlugin(DogePlugin):
                 return 'lib' + name + '.dll'
             elif type is BinaryType.EXECUTABLE:
                 return name + '.exe'
+
+    @staticmethod
+    def _ensure_directory_exists(file_path: str):
+        dir_path = os.path.dirname(file_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
