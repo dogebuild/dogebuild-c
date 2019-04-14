c6714931e3016dd2456061135e57116295ed4747
Kirill Sulim <kirillsulim@gmail.com>
2019-04-14 17:36:20 +0500

    Extract gcc logic in separate class

--- src/main/dogebuild_c/c_plugin.py
+++ src/main/dogebuild_c/c_plugin.py
@@ -1,36 +1,27 @@
 from subprocess import call
-from enum import Enum, unique, auto
 from dogebuild.plugins import DogePlugin
 import os
 import shutil
 from typing import List, Tuple
 
-
-@unique
-class BinaryType(Enum):
-    STATIC_LIBRARY = auto()
-    DYNAMIC_LIBRARY = auto()
-    EXECUTABLE = auto()
+from dogebuild_c.gcc_wrapper import GccWrapper, BinaryType
 
 
 class CPlugin(DogePlugin):
     NAME = 'c-plugin'
 
-    ALLOWED_CODE_EXTENSIONS = [
-        '.c',
-    ]
-
-    ALLOWED_HEADER_EXTENSIONS = [
-        '.h',
-    ]
-
     def __init__(self, **kwargs):
         super(CPlugin, self).__init__()
 
+        self.gcc = GccWrapper()
+
         self.add_task('compile', self.compile, phase='compile')
 
+        self.add_task('test', self.test, phase='test')
+        self.add_dependency('test', ['compile'])
+
         self.add_task('link', self.link, phase='link')
-        self.add_dependency('link', ['compile'])
+        self.add_dependency('link', ['test'])
 
         self.add_task('run', self.run, phase='run')
         self.add_dependency('run', ['link'])
@@ -38,34 +29,66 @@ class CPlugin(DogePlugin):
         self.add_task('clean', self.clean, phase='clean')
 
         self.out = kwargs.get('out', 'a')
+        self.out_file = None
+        self.test_out = kwargs.get('test_out', 'test')
         self.src = kwargs.get('src')
         self.headers = kwargs.get('headers', [])
+        self.test_src = kwargs.get('test_src', [])
         self.type = kwargs.get('type', BinaryType.STATIC_LIBRARY)
         self.build_dir = kwargs.get('build_dir', 'build')
+        self.test_build_dir = kwargs.get('test_build_dir', 'test_build')
         self.o_files = []
+        self.test_exclude = kwargs.get('test_exclude', [])
 
     def compile(self) -> int:
-        code, o_files = self._compile(self.build_dir, self.src, self.type)
+        code, o_files = self.gcc.compile(self.build_dir, self.type, self.src, [])
         if code:
             return code
         else:
             self.o_files = o_files
             return 0
 
-    def link(self) -> int:
-        code = self._link(self.build_dir, self.o_files, self.out, self.type)
+    def test(self) -> int:
+        if len(self.test_src) == 0:
+            return 0
+
+        self.gcc.copy_header(self.build_dir, self.headers)
+
+        code, test_o_files = self.gcc.compile(self.test_build_dir, BinaryType.EXECUTABLE, self.test_src, [os.path.join(self.build_dir, 'headers')])
         if code:
             return code
 
+        # Fixme
+        exclude_o_files = []
+        for src in self.src:
+            if src in self.test_exclude:
+                base, extension = os.path.splitext(src)
+                if extension in GccWrapper.ALLOWED_CODE_EXTENSIONS:
+                    src = base + '.o'
+                file_path = os.path.join(self.build_dir, src)
+                exclude_o_files.append(file_path)
+
+        code, test_out_file = self.gcc.link(self.test_build_dir, list(set(self.o_files) - set(exclude_o_files)) + test_o_files, self.test_out, BinaryType.EXECUTABLE)
+        if code:
+            return code
+
+        return call([test_out_file])
+
+    def link(self) -> int:
+        code, out_file = self.gcc.link(self.build_dir, self.o_files, self.out, self.type)
+        if code:
+            return code
+
+        self.out_file = out_file
+
         if self.type in [BinaryType.STATIC_LIBRARY, BinaryType.DYNAMIC_LIBRARY]:
-            self._copy_header(self.build_dir, self.headers)
+            self.gcc.copy_header(self.build_dir, self.headers)
 
         return 0
 
     def run(self) -> int:
         if self.type is BinaryType.EXECUTABLE:
-            out_file = os.path.join('.', self.build_dir, CPlugin._resolve_out_name(self.type, self.out))
-            command = [out_file]
+            command = [self.out_file]
             return call(command)
         else:
             print('Type {} is not executable'.format(self.type))
@@ -73,92 +96,4 @@ class CPlugin(DogePlugin):
 
     def clean(self):
         shutil.rmtree(self.build_dir)
-
-    @staticmethod
-    def _compile(build_dir: str, src_list: List[str], type: BinaryType) -> Tuple[int, List[str]]:
-        if not os.path.exists(build_dir):
-            os.makedirs(build_dir)
-
-        command = ['gcc']
-
-        o_files = []
-
-        for src in src_list:
-            command.append('-c')
-            if type is BinaryType.DYNAMIC_LIBRARY:
-                # Need to create macros in binary code.
-                # See https://renenyffenegger.ch/notes/development/languages/C-C-plus-plus/GCC/options/f/pic/index
-                command.append('-fPIC')
-            command.append(src)
-            command.append('-o')
-
-            base, extension = os.path.splitext(src)
-
-            if extension in CPlugin.ALLOWED_CODE_EXTENSIONS:
-                src = base + '.o'
-            else:
-                print('Warn: not allowed code file extension {} in file {}'.format(extension, src))
-
-            file_path = os.path.join(build_dir, src)
-            CPlugin._ensure_directory_exists(file_path)
-
-            command.append(file_path)
-            o_files.append(file_path)
-
-        return call(command), o_files
-
-    @staticmethod
-    def _copy_header(build_dir: str, header_list: List[str]):
-        for header in header_list:
-            base, extension = os.path.splitext(header)
-            if extension not in CPlugin.ALLOWED_HEADER_EXTENSIONS:
-                print('Warn: not allowed header file extension {} in file {}'.format(extension, header))
-
-            file_path = os.path.join(build_dir, 'headers', header)
-            CPlugin._ensure_directory_exists(file_path)
-
-            shutil.copyfile(header, file_path)
-
-    @staticmethod
-    def _link(build_dir: str, o_files: List[str], out_name: str, type: BinaryType) -> int:
-        if type is BinaryType.STATIC_LIBRARY:
-            out_file = os.path.join(build_dir, CPlugin._resolve_out_name(type, out_name))
-            command = ['ar', '-rcs', out_file, *o_files]
-            return call(command)
-
-        elif type is BinaryType.DYNAMIC_LIBRARY:
-            out_file = os.path.join(build_dir, CPlugin._resolve_out_name(type, out_name))
-            command = ['gcc', '-shared', *o_files, '-o', out_file]
-            return call(command)
-
-        elif type is BinaryType.EXECUTABLE:
-            out_file = os.path.join(build_dir, CPlugin._resolve_out_name(type, out_name))
-            command = ['gcc', *o_files, '-o', out_file]
-            return call(command)
-
-        else:
-            raise NotImplementedError('Unknown type {}'.format(type))
-
-    @staticmethod
-    def _resolve_out_name(type: BinaryType, name: str):
-        if os.name is 'posix':
-            if type is BinaryType.STATIC_LIBRARY:
-                return 'lib' + name + '.a'
-            elif type is BinaryType.DYNAMIC_LIBRARY:
-                return 'lib' + name + '.so'
-            elif type is BinaryType.EXECUTABLE:
-                return name
-        else:
-            if type is BinaryType.STATIC_LIBRARY:
-                return 'lib' + name + '.lib'
-            elif type is BinaryType.DYNAMIC_LIBRARY:
-                return 'lib' + name + '.dll'
-            elif type is BinaryType.EXECUTABLE:
-                return name + '.exe'
-
-    @staticmethod
-    def _ensure_directory_exists(file_path: str):
-        dir_path = os.path.dirname(file_path)
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-
+        shutil.rmtree(self.test_build_dir)
---
+++ src/main/dogebuild_c/gcc_wrapper.py
@@ -0,0 +1,113 @@
+from subprocess import call
+from enum import Enum, unique, auto
+import os
+import shutil
+from typing import List, Tuple
+
+
+@unique
+class BinaryType(Enum):
+    STATIC_LIBRARY = auto()
+    DYNAMIC_LIBRARY = auto()
+    EXECUTABLE = auto()
+
+
+class GccWrapper:
+    ALLOWED_CODE_EXTENSIONS = [
+        '.c',
+    ]
+
+    ALLOWED_HEADER_EXTENSIONS = [
+        '.h',
+    ]
+
+    def compile(self, build_dir: str, type: BinaryType, src_list: List[str], headers_dirs: List[str]) -> Tuple[int, List[str]]:
+        if not os.path.exists(build_dir):
+            os.makedirs(build_dir)
+
+        o_files = []
+
+        for src in src_list:
+            command = ['gcc', '-c']
+
+            for header_dir in headers_dirs:
+                command.append('-I{}'.format(header_dir))
+
+            if type is BinaryType.DYNAMIC_LIBRARY:
+                # Need to create macros in binary code.
+                # See https://renenyffenegger.ch/notes/development/languages/C-C-plus-plus/GCC/options/f/pic/index
+                command.append('-fPIC')
+            command.append(src)
+            command.append('-o')
+
+            base, extension = os.path.splitext(src)
+
+            if extension in GccWrapper.ALLOWED_CODE_EXTENSIONS:
+                src = base + '.o'
+            else:
+                print('Warn: not allowed code file extension {} in file {}'.format(extension, src))
+
+            file_path = os.path.join(build_dir, src)
+            _ensure_directory_exists(file_path)
+
+            command.append(file_path)
+            o_files.append(file_path)
+
+            code = call(command)
+            if code:
+                return code, []
+
+        return 0, o_files
+
+    def copy_header(self, build_dir: str, header_list: List[str]):
+        for header in header_list:
+            base, extension = os.path.splitext(header)
+            if extension not in self.ALLOWED_HEADER_EXTENSIONS:
+                print('Warn: not allowed header file extension {} in file {}'.format(extension, header))
+
+            file_path = os.path.join(build_dir, 'headers', header)
+            _ensure_directory_exists(file_path)
+
+            shutil.copyfile(header, file_path)
+
+    def link(self, build_dir: str, o_files: List[str], out_name: str, type: BinaryType) -> Tuple[int, str]:
+        if type is BinaryType.STATIC_LIBRARY:
+            out_file = os.path.join(build_dir, self._resolve_out_name(type, out_name))
+            command = ['ar', '-rcs', out_file, *o_files]
+            return call(command), out_file
+
+        elif type is BinaryType.DYNAMIC_LIBRARY:
+            out_file = os.path.join(build_dir, self._resolve_out_name(type, out_name))
+            command = ['gcc', '-shared', *o_files, '-o', out_file]
+            return call(command), out_file
+
+        elif type is BinaryType.EXECUTABLE:
+            out_file = os.path.join(build_dir, self._resolve_out_name(type, out_name))
+            command = ['gcc', *o_files, '-o', out_file]
+            return call(command), out_file
+
+        else:
+            raise NotImplementedError('Unknown type {}'.format(type))
+
+    @staticmethod
+    def _resolve_out_name(type: BinaryType, name: str):
+        if os.name is 'posix':
+            if type is BinaryType.STATIC_LIBRARY:
+                return 'lib' + name + '.a'
+            elif type is BinaryType.DYNAMIC_LIBRARY:
+                return 'lib' + name + '.so'
+            elif type is BinaryType.EXECUTABLE:
+                return name
+        else:
+            if type is BinaryType.STATIC_LIBRARY:
+                return 'lib' + name + '.lib'
+            elif type is BinaryType.DYNAMIC_LIBRARY:
+                return 'lib' + name + '.dll'
+            elif type is BinaryType.EXECUTABLE:
+                return name + '.exe'
+
+
+def _ensure_directory_exists(file_path: str):
+    dir_path = os.path.dirname(file_path)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
